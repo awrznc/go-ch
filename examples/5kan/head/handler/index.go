44a35b37954058caf46021cb413348479ccb5eed
awrznc <awrznc@gmail.com>
2022-05-28 01:21:49 +0900

    Added Accept-Ranges.

--- examples/5kan/head/handler/index.go
+++ examples/5kan/head/handler/index.go
@@ -3,11 +3,30 @@ package handler
 import (
 	"net/http"
 	"time"
+	"compress/gzip"
+	"strings"
+	"bytes"
 )
 
 func Handler(writer http.ResponseWriter, request *http.Request) {
-	writer.Write([]byte(`<p>5kan</p>`))
+	var result string
+	contents := `<p>5kan</p>`
+	if strings.Contains(request.Header.Get("Accept-Encoding"), "gzip") {
+		result = gzipping(contents)
+		writer.Header().Set("Content-Encoding", "gzip")
+	} else {
+		result = contents
+	}
+	writer.Write([]byte(result))
 	writer.Header().Set(`Content-Type`, `text/plain`)
 	writer.Header().Set(`Accept-Ranges`, `bytes`)
 	writer.Header().Set(`Last-Modified`, time.Now().Format("Mon, 02 Jan 2006 15:04:05 MST"))
+}
+
+func gzipping(target string) string {
+	var buffer bytes.Buffer
+	writer := gzip.NewWriter(&buffer)
+	writer.Write([]byte(target))
+	writer.Close()
+	return string(buffer.Bytes())
 }
