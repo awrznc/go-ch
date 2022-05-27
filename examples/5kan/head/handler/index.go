c909d296d64af2868abaefead87d6e6586d7fc59
awrznc <awrznc@gmail.com>
2022-05-28 01:08:52 +0900

    Added Last-Modified.

--- examples/5kan/head/handler/index.go
+++ examples/5kan/head/handler/index.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"net/http"
+	"time"
 )
 
 func Handler(writer http.ResponseWriter, request *http.Request) {
 	writer.Write([]byte(`<p>5kan</p>`))
 	writer.Header().Set(`Content-Type`, `text/plain`)
+	writer.Header().Set(`Last-Modified`, time.Now().Format("Mon, 02 Jan 2006 15:04:05 MST"))
 }
