455fdc49ec801cd11cf78e22a0205ede5412d861
awrznc <awrznc@gmail.com>
2022-05-28 01:12:51 +0900

    Added Accept-Ranges.

--- examples/5kan/head/handler/index.go
+++ examples/5kan/head/handler/index.go
@@ -8,5 +8,6 @@ import (
 func Handler(writer http.ResponseWriter, request *http.Request) {
 	writer.Write([]byte(`<p>5kan</p>`))
 	writer.Header().Set(`Content-Type`, `text/plain`)
+	writer.Header().Set(`Accept-Ranges`, `bytes`)
 	writer.Header().Set(`Last-Modified`, time.Now().Format("Mon, 02 Jan 2006 15:04:05 MST"))
 }
